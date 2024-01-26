4cd19b6653696d4a06fb65a30c122a19c74880f3
Farhan Asif <farhanmatics@gmail.com>
2024-01-26 01:25:53 +0000

    basic UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        tourista: '#FFCB0B',
+        viacolor: '#E48FA0',
+        tourPurple: '#7039aa',
+        lightPurple: '#8a52ba',
+        blueSky: '#79def2',
+        darkSky: '#1c0f36',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
 
